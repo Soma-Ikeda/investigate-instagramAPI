3cddad7cf0c928331d459cd0c3c774308b88c54c
Soma_Ikeda <soma.ikeda@80and.co>
2023-04-27 16:06:02 -0700

    added generated token

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,9 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+async function fetchImages() {
+  console.log(process.env.NEXT_PUBLIC_INSTAGRAM_ACCESS_TOKEN);
+  const response = await axios.get(
+    `https://graph.instagram.com/me/media?fields=id,caption,media_url,thumbnail_url&access_token=${process.env.NEXT_PUBLIC_INSTAGRAM_ACCESS_TOKEN}`
+  );
+  return response.data.data;
+}
 
 const IndexPage = () => {
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    async function fetchAndSetImages() {
+      const fetchedImages = await fetchImages();
+      console.log(fetchedImages);
+      setImages(fetchedImages.slice(0, 4));
+    }
+
+    fetchAndSetImages();
+  }, []);
+
+  console.log("sss");
+
   return (
     <div>
-      <h1>This is about page!</h1>
+      {images.map((image: any) => (
+        <img
+          key={image.id}
+          src={image.media_url || image.thumbnail_url}
+          alt={image.caption}
+          style={{
+            width: "200px",
+            height: "200px",
+          }}
+        />
+      ))}
     </div>
   );
 };
